8b2fff4df8f741fbdb49bd46cac2b6078abb46a6
Shaun Fattig <shfattig@gmail.com>
2026-05-14 15:26:03 -0500

    fix: add explicit flutter/foundation.dart import for ValueListenable

--- super_editor/lib/src/default_editor/paragraph.dart
+++ super_editor/lib/src/default_editor/paragraph.dart
@@ -1,4 +1,5 @@
 import 'package:attributed_text/attributed_text.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:super_editor/src/core/document.dart';
--- super_editor/lib/src/default_editor/text.dart
+++ super_editor/lib/src/default_editor/text.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 
 import 'package:attributed_text/attributed_text.dart';
 import 'package:collection/collection.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart' hide SelectableText;
 import 'package:flutter/services.dart';
 import 'package:super_editor/src/core/document.dart';
